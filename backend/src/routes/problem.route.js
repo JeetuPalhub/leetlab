6c8f52634868d1f9ee68ed3be77230a9d85fe862
JeetuPalhub <jeetupal.pal31@gmail.com>
2025-09-27 23:08:07 +0530

    problem controller added

--- backend/src/routes/problem.route.js
+++ backend/src/routes/problem.route.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { authMiddleware, checkAdmin } from "../middleware/auth.middleware";
 
 
 const problemRoutes = express.Router();
 
-
+problemRoutes.post("/create-problem", authMiddleware, checkAdmin, createProblem)
 
 
 export default problemRoutes;
